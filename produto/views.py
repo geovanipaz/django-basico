2ace7d8f397d6d335d52ed7cb66e2b2ec321a14e
Geovani Paz <geovanipaz7@gmail.com>
2022-02-19 09:49:13 -0300

    ultimo commit

--- produto/views.py
+++ produto/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Olá produtos')
+    return render(request,'produto/index.html')
 
 
--- projeto/settings.py
+++ projeto/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'produto.apps.ProdutoConfig',
+    'sobre.apps.SobreConfig',
 ]
 
 MIDDLEWARE = [
